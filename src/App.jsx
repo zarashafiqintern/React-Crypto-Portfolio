4483172fc7bdd3634ab786cdf8e7ecb1d6845faa
Zara Shafiq <zarashafiq.intern@devsinc.com>
2025-11-12 16:07:21 +0500

    feat: add Outlet for rendering routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { PortfolioProvider } from "./context/PortfolioContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Investments from "./pages/investments/Investments";
 import EditInvestment from "./pages/EditInvestment/EditInvestment";
 import CreateInvestment from "./pages/createinvestment/CreateInvestment";
+import Layout from "./layout/Layout";
 import "./App.css";
 
 function App() {
   return (
     <PortfolioProvider>
       <Router>
-        <div className="app-container">
-          <Navbar />
-          <Header
-            icon="💰" 
-            title="Crypto Portfolio Tracker"
-            subtitle="Track your cryptocurrency investments with live Binance prices"
-          />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/investments" element={<Investments />} />
-            <Route path="/investments/:id/edit" element={<EditInvestment />} />
-            <Route path="/investment/create" element={<CreateInvestment />} />
-          </Routes>
-        </div>
+        <Routes>
+         
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="investments" element={<Investments />} />
+            <Route path="investments/:id/edit" element={<EditInvestment />} />
+            <Route path="investment/create" element={<CreateInvestment />} />
+          </Route>
+        </Routes>
       </Router>
     </PortfolioProvider>
   );
